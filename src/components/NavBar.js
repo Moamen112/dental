8c458ced535da61f88545cdd8fb67f2b4a87b11e
Moamen Shehab <memo.shehab30@gmail.com>
2023-10-05 17:30:14 +0300

    Fixing error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,7 +26,7 @@ const NavBar = () => {
 			<NavList className={`menu ${mobileMenuVisible ? "show" : ""}`}>
 				<NavItem
 					exact
-					to="https://moamen112.github.io/dental/"
+					to="dental"
 					activeClassName="active">
 					Home
 				</NavItem>
