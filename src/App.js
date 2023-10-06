c513fa8f0802217cbdcc5f3c2c1700daf6437804
Moamen Shehab <memo.shehab30@gmail.com>
2023-10-06 13:57:06 +0300

    Fiing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import GlobalStyles from "./components/GlobalStyles"; // Import the GlobalStyles component
 import NavBar from "./components/NavBar";
